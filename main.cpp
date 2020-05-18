c3e1bc7f22d664dee4a21131882707ebe0e2a0c6
juha tervo <juha.tervo@aalto.fi>
2020-05-18 19:14:47 +0300

    missing }

--- main.cpp
+++ main.cpp
@@ -16,27 +16,71 @@ double robotXOffset = mapWidth / 5, robotYOffset = mapHeight / 3;
 // Defining an l vector to store the log odds values of each cell
 vector<vector<double>> l(mapWidth / gridWidth, vector<double>(mapHeight / gridHeight));
 
-double inverseSensorModel(double x, double y, double theta, double xi, double yi, double sensorData[])
+double inverseSensorModel(
+    double x,           // robot pose x
+    double y,           // robot pose y
+    double theta,       // robot pose
+    double xi,          // center of a mass for cell x
+    double yi,          // center of a mass for cell y
+    double sensorData[] // measurement data
+    )
 {
-    // You will be coding this section in the upcoming concept!
-    return 0.4;
+    //******************Code the Inverse Sensor Model Algorithm**********************//
+    // Defining Sensor Characteristics
+    double Zk, thetaK, sensorTheta;
+    double minDelta = -1;
+    double alpha = 200, beta = 20;
+
+    //******************Compute r and phi**********************//
+    double r = sqrt(pow(xi - x, 2) + pow(yi - y, 2));
+    double phi = atan2(yi - y, xi - x) - theta;
+
+    //Scaling Measurement to [-90 -37.5 -22.5 -7.5 7.5 22.5 37.5 90]
+    for (int i = 0; i < 8; i++)
+    {
+        if (i == 0)
+        {
+            sensorTheta = -90 * (M_PI / 180);
+        }
+        else if (i == 1)
+        {
+            sensorTheta = -37.5 * (M_PI / 180);
+        }
+        else if (i == 6)
+        {
+            sensorTheta = 37.5 * (M_PI / 180);
+        }
+        else if (i == 7)
+        {
+            sensorTheta = 90 * (M_PI / 180);
+        }
+        else
+        {
+            sensorTheta = (-37.5 + (i - 1) * 15) * (M_PI / 180);
+        }
+
+        if (fabs(phi - sensorTheta) < minDelta || minDelta == -1)
+        {
+            Zk = sensorData[i];
+            thetaK = sensorTheta;
+            minDelta = fabs(phi - sensorTheta);
+        }
+    }
 }
 
 void occupancyGridMapping(double Robotx, double Roboty, double Robottheta, double sensorData[])
 {
-    //1 - TODO: Generate a grid (size 300x150) and then loop through all the cells
-    //2- TODO: Compute the center of mass of each cell xi and yi
-    //double xi = x * gridWidth + gridWidth / 2 - robotXOffset;
-    //double yi = -(y * gridHeight + gridHeight / 2) + robotYOffset;
-    //3- TODO: Check if each cell falls under the perceptual field of the measurements
     //******************Code the Occupancy Grid Mapping Algorithm**********************//
 
+    //1 - Generate a grid (size 300x150) and then loop through all the cells
     for (int x = 0; x < mapWidth / gridWidth; x++)
     {
         for (int y = 0; y < mapHeight / gridHeight; y++)
         {
+            //2 - Compute the center of mass of each cell xi and yi
             double xi = x * gridWidth + gridWidth / 2 - robotXOffset;
             double yi = -(y * gridHeight + gridHeight / 2) + robotYOffset;
+            //3 -  Check if each cell falls under the perceptual field of the measurements
             if (sqrt(pow(xi - Robotx, 2) + pow(yi - Roboty, 2)) <= Zmax)
             {
                 l[x][y] = l[x][y] + inverseSensorModel(Robotx, Roboty, Robottheta, xi, yi, sensorData) - l0;
@@ -55,7 +99,8 @@ int main()
     FILE *measurementFile = fopen("measurement.txt", "r");
 
     printf("ogma loop starting \n");
-    if (posesFile == NULL || measurementFile == NULL) {
+    if (posesFile == NULL || measurementFile == NULL)
+    {
         printf("open input file fails\n");
         return -1;
     }
@@ -68,6 +113,7 @@ int main()
         {
             fscanf(measurementFile, "%lf", &measurementData[i]);
         }
+        // pose is x,y,theta (theta x.x degrees -> transformed to rad)
         occupancyGridMapping(robotX, robotY, (robotTheta / 10) * (M_PI / 180), measurementData);
     }
 
--- readme.md
+++ readme.md
@@ -1,3 +1,22 @@
 # Occumancy Grid Mapping Algorithm
+
 for robotic sw engineering course
 
+```cpp
+double inverseSensorModel(double x, double y, double theta, double xi, double yi, double sensorData[])
+```
+
+
+Summary of notations for the sonar rangefinder inverse sensor model
+
+mi​: Map at instant i or current cell that is being processed
+
+xi,yix_{i},y_{i}xi​,yi​: Center of mass of the current cell mi
+
+r: Range of the center of mass computed with respect to robot pose and center of mass
+
+k: The sonar rangefinder cone that best aligns with the cell being considered computed with respect to the robot pose (x,y,θ\thetaθ), center of mass (xi,yi), and sensor angle.
+
+β\betaβ: Opening angle of the conical region formed out of the measurement beams.
+
+α\alphaα: Width of obstacles which is almost equal to the size of a cell. Please not that alpha is not the width of the conical region as the video mention but instead it's the width of a cell. 
